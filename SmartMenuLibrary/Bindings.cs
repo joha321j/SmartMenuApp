c799985cb6cadae0548d88c0137ea2f38d823168
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-10-01 11:54:42 +0200

    Implement Bindings.Call method.

--- SmartMenuLibrary/Bindings.cs
+++ SmartMenuLibrary/Bindings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FunctionLibrary;
 
 namespace SmartMenuLibrary
 {
@@ -10,7 +11,26 @@ namespace SmartMenuLibrary
     {
         internal static void Call(string callId)
         {
-            Console.WriteLine(callId);
+            switch(callId)
+            {
+                case "linie1":
+                    Console.WriteLine(Functions.DoThis());
+                    break;
+                case "linie2":
+                    Console.WriteLine(Functions.DoThat());
+                    break;
+                case "linie3":
+                    Console.WriteLine("Tell me what to do.");
+                    string userInput = Console.ReadLine();
+                    Console.WriteLine(Functions.DoSomething(userInput));
+                    break;
+                case "linie4":
+                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                    break;
+                default:
+                    Console.WriteLine("This is the default switch");
+                    break;
+            }
         }
     }
 }
